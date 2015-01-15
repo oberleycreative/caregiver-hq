cf3fcd72ed67d9e20313e2b166edc38335333f97
oberleycreative <tyler.oberley@gmail.com>
2015-01-15 16:36:22 -0500

    adding slide menu plugin

---
+++ wp-content/side-slide-responsive-menu/sideslidemenu.php
@@ -0,0 +1,16 @@
+<?php   
+/*
+Plugin Name: Side Slide Menu
+Plugin URI: http://codecanyon.net/user/wp_workshop
+Description: Modern and easy to use support helpdesk solution
+Author: WP Workshop
+Version: 1.0
+Author URI: http://codecanyon.net/user/wp_workshop
+Licence: GPLv2
+*/
+
+require dirname( __FILE__ ) . '/SideSlideMenu.class.php';
+
+SideSlideMenu::init(__FILE__);
+
+?>

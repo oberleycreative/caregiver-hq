cbb00edfc84ef71fb4b3460f71d75e47515c7a83
oberleycreative <tyler.oberley@gmail.com>
2015-01-15 16:12:49 -0500

    update

--- wp-content/themes/responsive/front-page.php
+++ wp-content/themes/responsive/front-page.php
@@ -11,21 +11,16 @@ if ( !defined( 'ABSPATH' ) ) {
 
 
 <div class="super-hero">
- 	<!-- <img src="wp-content/themes/pr2014/images/tem-homebknd.jpg">  --><!-- place for main hero image. img for placeholder but use php or whatever for final -->
-
- 
+ 	
 
 <div class="super-hero-inner">
 		<div class="super-hero-callout">
-			<h1>Every child thrives.<br><span class="every-child">Together, we pave the path for their success.</span></h1>
-			<p>Since 1832, Pressley Ridge has understood that all children can change and grow and that all family can use support. We help over 5,200 children and families annually through Educational Opportunities, Foster Care Connections, Residential Options, and Community-based Support with locations in Pennsylvania, Delaware, Maryland, Ohio, Virginia, West Virginia, as well as internationally in Portugal and Hungary.</p>
-			<div class="super-hero-links">Hear Our Story &#8226; Become a Parent &#8226; Donate Today</div>
+			<h5>Your parents didn't raise a caregiver.</span></h5>
+			<h1>They raised a person who cared.</span></h1>
 		</div>
 </div>
 
-	<div class="our-mission">
-		<div class="our-mission-inner"><h4>Our Misson is to</h4>&nbsp;<em> improve the lives of children and families everywhere and especially those whose lives we touch.</em></div>
-	</div>
+
 </div>
 
 

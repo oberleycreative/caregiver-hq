de0fc6f4ee3556fd39b466d8fa215bfda1ca07d0
oberleycreative <tyler.oberley@gmail.com>
2015-01-20 10:55:49 -0500

    css catch up

--- wp-content/themes/Caregiver-HQ/home.php
+++ wp-content/themes/Caregiver-HQ/home.php
@@ -31,28 +31,24 @@ $more = 0;
 <div class="container-thirds">
 
 <div class="third">
-	<img src="http://localhost:8888/caregiver-hq/wp-content/themes/Caregiver-HQ/images/temp_icon.jpg" alt="" title="">
-	<div class="title"><h5>Member Resources</h5></div>
-
+	<div class="title"><h5>Free Trial</h5></div>
 	<div class="block">
-	<p>Here, you have access to our full collection of benefits: the CaregiverHQ newsletter, our own blog full of tips and inspiration, fact sheets, how-tos, and a wide variety of resources designed to help and support you.</p>
+	<p>Proin non purus pharetra, tincidunt ligula ut, imperdiet nulla. Aenean turpis tortor, tempor ut rutrum sit amet, cursus eget sem. Proin laoreet nulla vitae ipsum varius rhoncus.</p>
 	</div>
 </div>
 
 
 <div class="third">
-	<img src="http://localhost:8888/caregiver-hq/wp-content/themes/Caregiver-HQ/images/temp_icon.jpg" alt="" title=""><p></p>
-	<div class="title"><h5>Personal Care Services</h5></div>
+	<div class="title"><h5>Standard</h5></div>
 	<div class="block">
-	<p>Does your loved one require in-home care beyond what you are able to provide alone? Trust CaregiverHQ to find high-quality personal care options for you.</p>
+	<p>Proin non purus pharetra, tincidunt ligula ut, imperdiet nulla. Aenean turpis tortor, tempor ut rutrum sit amet, cursus eget sem. Proin laoreet nulla vitae ipsum varius rhoncus.</p>
 	</div>
 </div>
 
 <div class="third">
-	<img src="http://localhost:8888/caregiver-hq/wp-content/themes/Caregiver-HQ/images/temp_icon.jpg" alt="" title=""><p></p>
-	<div class="title"><h5>Legal</h5></div>
+	<div class="title"><h5>Premium</h5></div>
 	<div class="block">
-	<p>Whether you need a legal services for estate planning, writing a will, or just getting your loved one’s affairs in order – let our premium network of legal professionals help you.</p>
+	<p>Proin non purus pharetra, tincidunt ligula ut, imperdiet nulla. Aenean turpis tortor, tempor ut rutrum sit amet, cursus eget sem. Proin laoreet nulla vitae ipsum varius rhoncus.</p>
 	</div>
 </div>
 

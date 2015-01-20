38fa5cab0a08be420250f75aff89d571b281ffba
oberleycreative <tyler.oberley@gmail.com>
2015-01-20 11:10:49 -0500

    front page catch up

--- wp-content/themes/Caregiver-HQ/home.php
+++ wp-content/themes/Caregiver-HQ/home.php
@@ -41,6 +41,7 @@ $more = 0;
 
 <div class="third">
 	<div class="title"><h5>Standard</h5></div>
+	<img src="http://localhost:8888/caregiver-hq/wp-content/themes/Caregiver-HQ/images/test_icon.png" class="service-icon" alt="" title="">
 	<div class="block">
 	<p>Proin non purus pharetra, tincidunt ligula ut, imperdiet nulla. Aenean turpis tortor, tempor ut rutrum sit amet, cursus eget sem. Proin laoreet nulla vitae ipsum varius rhoncus.</p>
 	</div>
@@ -48,6 +49,7 @@ $more = 0;
 
 <div class="third">
 	<div class="title"><h5>Premium</h5></div>
+	<img src="http://localhost:8888/caregiver-hq/wp-content/themes/Caregiver-HQ/images/test_icon.png" class="service-icon" alt="" title="">
 	<div class="block">
 	<p>Proin non purus pharetra, tincidunt ligula ut, imperdiet nulla. Aenean turpis tortor, tempor ut rutrum sit amet, cursus eget sem. Proin laoreet nulla vitae ipsum varius rhoncus.</p>
 	</div>
@@ -68,6 +70,8 @@ $more = 0;
 						<div class="featured-resources-inner">
 							<div class="section-title"><h1>Featured Resources</h1></div>
 								<!-- Posts Carousel goes here -->
+								<img src="http://localhost:8888/caregiver-hq/wp-content/themes/Caregiver-HQ/images/test-feed.png" alt="" title="">
+
 							<div class="btn-home-services"><a href="#" alt="">View all resources</a></div>
 						</div>
 
